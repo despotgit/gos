2c65a6246291375c96b2db3554e220a50c8ff6d4
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2020-05-18 09:45:03 +0200

    Add backend from udemy.

---
+++ gos-backend/config/secret.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //url: "mongodb+srv://admin:3SSFMGo761gaasv@cluster0-9y0vx.mongodb.net/test?retryWrites=true&w=majority",
+  url: "mongodb://127.0.0.1:27017/chat",
+  secret: "13243546"
+};
