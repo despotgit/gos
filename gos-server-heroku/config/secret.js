61bb6dfbaa1c1c6c95b0214dbc8c911ce44e804d
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2020-05-21 00:06:27 +0200

    WOrking on it.

--- gos-server-heroku/config/secret.js
+++ gos-server-heroku/config/secret.js
@@ -1,5 +1,8 @@
 module.exports = {
+  url:
+    "mongodb+srv://admin:3SSFMGo761gaasv@cluster0-9y0vx.mongodb.net/admin?replicaSet=Cluster0-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=Cluster0-shard-0&3t.databases=admin,test",
   //url: "mongodb+srv://admin:3SSFMGo761gaasv@cluster0-9y0vx.mongodb.net/test?retryWrites=true&w=majority",
-  url: "mongodb://127.0.0.1:27017/chat",
+
+  //url: "mongodb://127.0.0.1:27017/chat",
   secret: "13243546"
 };
