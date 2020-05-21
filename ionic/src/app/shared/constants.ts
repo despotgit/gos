b08a9276354f8d200ee4656d35a56ea8c0bfdfc0
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2020-05-21 16:38:49 +0200

    Target gos-server on heroku as node server.

--- ionic/src/app/shared/constants.ts
+++ ionic/src/app/shared/constants.ts
@@ -1,3 +1,3 @@
-export const serverUrl = "http://localhost:3000";
+//export const serverUrl = "http://localhost:3000";
 
-//export const serverUrl = "https://gos-server.herokuapp.com";
+export const serverUrl = "https://gos-server.herokuapp.com";
