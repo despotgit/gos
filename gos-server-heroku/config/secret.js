66b10e846648944acabe869a7f96d0f9148c18b9
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2020-05-21 13:03:23 +0200

    Add visual studio code stuff, change apostrophe, add correct secret for the mongo db.

--- gos-server-heroku/config/secret.js
+++ gos-server-heroku/config/secret.js
@@ -1,8 +1,6 @@
 module.exports = {
-  url:
-    "mongodb+srv://admin:3SSFMGo761gaasv@cluster0-9y0vx.mongodb.net/admin?replicaSet=Cluster0-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=Cluster0-shard-0&3t.databases=admin,test",
-  //url: "mongodb+srv://admin:3SSFMGo761gaasv@cluster0-9y0vx.mongodb.net/test?retryWrites=true&w=majority",
-
-  //url: "mongodb://127.0.0.1:27017/chat",
-  secret: "13243546"
+    //url: "mongodb+srv://admin:132435@cluster0-vuzyl.mongodb.net/test?retryWrites=true&w=majority"
+    url: "mongodb+srv://admin:3SSFMGo761gaasv@cluster0-9y0vx.mongodb.net/test?retryWrites=true&w=majority",
+    //url: "mongodb://127.0.0.1:27017/chat",
+    secret: "13243546"
 };
