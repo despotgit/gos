5bf3ad34edbc18727c1f364ae10ecde7a9ec4d02
Vladimir Despotovic <despotovic_vladimir@yahoo.ie>
2020-05-24 00:18:30 +0200

    Commit message.

--- server/config/secret.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    //url: "mongodb+srv://admin:132435@cluster0-vuzyl.mongodb.net/test?retryWrites=true&w=majority"
-    url: "mongodb+srv://admin:3SSFMGo761gaasv@cluster0-9y0vx.mongodb.net/test?retryWrites=true&w=majority",
-    //url: "mongodb://127.0.0.1:27017/chat",
-    secret: "13243546"
-};
